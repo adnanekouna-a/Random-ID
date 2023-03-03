d9f9fbbf80bbabd41098d955373d3838758fc0a3
Adnane <adnanekouna.a@gmail.com>
2023-03-03 13:24:29 +0100

    Fix gender capitalization

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Functions
+const capitalize = (str) => {
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ");
+}
+
 // DOM variables
 const image = document.querySelector("#id-image")
 const first_name = document.querySelector("#first-name")
@@ -7,13 +16,13 @@ const gender = document.querySelector("#gender")
 const date_of_birth = document.querySelector("#dob")
 const phone = document.querySelector("#phone")
 
-
+// API handling
 fetch('https://randomuser.me/api/')
   .then(response => response.json())
   .then(data => {
     first_name.textContent = data.results[0].name.first
     last_name.textContent = data.results[0].name.last
-    gender.textContent = data.results[0].gender
+    gender.textContent = capitalize(data.results[0].gender)
     nationality.textContent = data.results[0].location.country
     date_of_birth.textContent = data.results[0].dob.date.slice(0,10)
     phone.textContent = data.results[0].phone
